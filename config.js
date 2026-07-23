68692aa7e783b360c26ec0e37ab4e8c86a18fb8a
KSamtoclef <samtoclef@gmail.com>
2026-07-23 12:10:19 +0100

    Add campaign configuration module

---
+++ config.js
@@ -0,0 +1,11 @@
+window.CAMPAIGN_CONFIG = Object.freeze({
+  amount: 30000,
+  currency: "₦",
+  image: "7F3FFD45-3D1C-4285-8EF5-A9071305839C.png",
+  storageKey: "wedding30kShareProgress",
+  offerUrls: {
+    continueUrl: "",
+    paymentUrl: "",
+    statusUrl: ""
+  }
+});
